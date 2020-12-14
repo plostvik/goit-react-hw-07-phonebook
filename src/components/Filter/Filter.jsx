6cea6a30448bbd4b206223fbee665845b9918f4c
Ostap Pluzhnik <plostvik@gmail.com>
2020-12-14 20:20:37 +0200

    add containers to forms

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,44 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Filter.module.css';
-import { connect } from 'react-redux';
-import { filterChange } from '../../redux/phonebook/phonebookActions';
 
-class Filter extends Component {
-  static propTypes = {
-    filterChange: PropTypes.func.isRequired,
-  };
-
-  state = {
-    filter: '',
-  };
-
-  handleFilterChange = e => {
-    const { filterChange } = this.props;
-    this.setState({ [e.target.name]: e.target.value });
-    filterChange(e.target.value);
-  };
-
-  render() {
-    return (
-      <div className={styles.wrapper}>
-        <label htmlFor="filter" className={styles.label}>
-          Find contacts by name
-        </label>
-        <input
-          type="text"
-          name="filter"
-          onChange={this.handleFilterChange}
-          value={this.state.filter}
-          className={styles.input}
-        />
-      </div>
-    );
-  }
+export default function Filter({ filter, filterChange }) {
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor="filter" className={styles.label}>
+        Find contacts by name
+      </label>
+      <input
+        type="text"
+        name="filter"
+        onChange={e => filterChange(e.target.value)}
+        value={filter}
+        className={styles.input}
+      />
+    </div>
+  );
 }
 
-const mapDispatchToProps = {
-  filterChange,
+Filter.propTypes = {
+  filterChange: PropTypes.func.isRequired,
 };
-
-export default connect(null, mapDispatchToProps)(Filter);
